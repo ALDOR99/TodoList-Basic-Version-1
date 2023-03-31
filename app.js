9a516eca5a6d51f041715e890fbe9ec3d2131bc2
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-03-31 18:40:15 +0300

    0.1

--- app.js
+++ app.js
@@ -5,18 +5,48 @@ const app = express();
 
 //-------------------------------------------------------------
 
-app.get("/", function (req, res) {
-  //   var a = 3 + 5;
-  //   res.send("Hello");
-  //res.send(a);
+app.set("view engine", "ejs");
 
+//-------------------------------------------------------------
+
+app.get("/", function (req, res) {
   var today = new Date();
   var currentDay = today.getDay();
-  if (currentDay === 4 || currentDay === 0) {
-    res.sendFile(__dirname + "/weekend.html");
-  } else {
-    res.sendFile(__dirname + "/weekday.html");
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+
+    case 1:
+      day = "Monday";
+      break;
+
+    case 2:
+      day = " Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+
+    case 4:
+      day = "Thursday";
+      break;
+
+    case 5:
+      day = "Friday";
+      break;
+
+    case 6:
+      day = "Saturday";
+      break;
+
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
   }
+
+  res.render("list", { kindOfDay: day });
 });
 
 //-------------------------------------------------------------
