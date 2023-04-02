010cd019de90936fdb1832ba5ade6ca8d2c28504
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-04-02 18:20:45 +0300

    0.4

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 //-------------------------------------------------------------
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+
+let workItems = [];
 //-------------------------------------------------------------
 
 app.set("view engine", "ejs");
@@ -29,15 +31,43 @@ app.get("/", function (req, res) {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 //-------------------------------------------------------------
 
 app.post("/", function (req, res) {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+
+    res.redirect("/work");
+  } else {
+    items.push(item);
+
+    res.redirect("/");
+  }
+});
+
+//-------------------------------------------------------------
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+//-------------------------------------------------------------
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+//-------------------------------------------------------------
+
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 //-------------------------------------------------------------
