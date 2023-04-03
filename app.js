55ae38fa8f03cfac1f1f204f4ca2d05004578b06
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-04-03 22:09:51 +0300

    0.6

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const date = require(__dirname + "/date.js");
+
 const app = express();
 
 //-------------------------------------------------------------
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
 
-let workItems = [];
+const workItems = [];
 //-------------------------------------------------------------
 
 app.set("view engine", "ejs");
@@ -22,13 +24,15 @@ app.use(express.static("public"));
 //-------------------------------------------------------------
 
 app.get("/", function (req, res) {
+  const day = date.getDate();
+
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 //-------------------------------------------------------------
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "Work") {
     workItems.push(item);
@@ -50,7 +54,7 @@ app.get("/work", function (req, res) {
 //-------------------------------------------------------------
 
 app.post("/work", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
